ea9d0cd9b44e35b9e77001967edfc5282dc6a816
Stepan Chatalyan <stepan.chatalyan@phiture.com>
2024-08-01 17:44:23 +0200

    move styles around

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type {Metadata} from "next";
-import "./globals.css";
+import "@/styles/globals.css";
 import ThemeToggle from "@/components/ThemeToggle";
 
 export const metadata: Metadata = {
